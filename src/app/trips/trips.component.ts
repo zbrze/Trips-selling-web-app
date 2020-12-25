2999078bc83615990ec5b50c2cc2a9cd7c79144b
zbrze <58784685+zbrze@users.noreply.github.com>
2020-12-26 00:48:25 +0100

    Add files via upload

--- src/app/trips/trips.component.ts
+++ src/app/trips/trips.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import {Trip} from '../trip'
 import { NgModule } from '@angular/core';
-import {AddTripService} from '../add-trip.service'
+import {AddTripService} from '../services/trip.service'
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { CommonModule } from '@angular/common';  
 import { BrowserModule } from '@angular/platform-browser';
+import { map } from 'rxjs/operators';
 @Component({
   selector: 'app-trips',
   templateUrl: './trips.component.html',
@@ -12,11 +13,10 @@ import { BrowserModule } from '@angular/platform-browser';
 })
 export class TripsComponent{
  tripList;
- maxID;
- cheapestID;
- cheapestPrice;
+ cheapID;
+ cheapPrice;
  expensivePrice;
- mExpensiveID;
+ expensiveID;
  showVar: boolean = false;
  searchText:string;
  fromDate: Date;
@@ -24,65 +24,39 @@ export class TripsComponent{
  country;
  tripToCart: Trip;
  tripsInCart: Trip[];
+
  toggleChild(){
      this.showVar = !this.showVar;
  }
  getDateFrom(event){
   this.fromDate = new Date(event.value);
-  console.log(this.fromDate);
 }
 getDateTo(event){
   this.toDate = new Date(event.value);
   console.log(this.toDate);
 }
-  constructor(service: AddTripService) { 
-    this.tripList = service.getTrip();
-    this.maxID = this.tripList.reduce((a, b) => a.id > b.id ? a : b).id;
-    this.onUpdate();
+
+  constructor(private service: AddTripService) { 
     this.tripsInCart = [];
-
+  }
+  ngOnInit(){
+    this.getTrips();
+    this.onUpdate();
   }
 
+  getTrips(){
+    this.service.getTrips().pipe(
+      map(c => c.map( val => 
+        ({id: val.payload.doc.id,...val.payload.doc.data()}))
+     )).
+    subscribe(x => this.tripList = x,err=>console.log(err));
+  }
   onUpdate(){
-    this.mExpensiveID = this.tripList.reduce((a, b) => a.price > b.price ? a : b).id;
-    this.expensivePrice = this.tripList.reduce((a, b) => a.price > b.price ? a : b).price;
-    this.cheapestID = this.tripList.reduce((a, b) => a.price < b.price ? a : b).id;
-    this.cheapestPrice = this.tripList.reduce((a, b) => a.price < b.price ? a : b).price;
+    this.service.getExpensive().subscribe(x => x.map(c => this.expensiveID = c.payload.doc.id));
+    this.service.getExpensive().subscribe(x => x.map(c => this.expensivePrice = c.payload.doc.data().price));
+    this.service.getCheap().subscribe(x => x.map(c => this.cheapID = c.payload.doc.id));
+    this.service.getCheap().subscribe(x => x.map(c => this.cheapPrice = c.payload.doc.data().price));
   }
 
 
-  onSubmit(trip: Trip){
-    this.tripList.push(trip);
-    this.tripList = Object.create(this.tripList);
-    this.onUpdate();
-  }
-
-  onDelete(ID: number){
-    this.tripList.splice(this.tripList.findIndex(x => x.id === ID), 1);
-    this.onUpdate();
-    this.tripsInCart.splice(this.tripsInCart.findIndex(x => x.id === ID), 1);
-  }
-
-  onBook(ID: number){
-    var bookedTrip = this.tripList.findIndex(x => x.id === ID);
-    this.tripList[bookedTrip].booked += 1;
-    this.tripToCart = this.tripList.filter(({id}) => id == ID);
-    var alreadyInCart = this.tripsInCart.filter(({id}) => id == ID);
-    if(!alreadyInCart[0]){
-      this.tripsInCart.push(this.tripToCart[0]);
-    }
-    
-    this.tripToCart[0].inCart +=1;
-  }
-  onResign(ID: number){
-    var bookedTrip = this.tripList.findIndex(x => x.id === ID);
-    this.tripList[bookedTrip].booked -= 1;
-    this.tripList[bookedTrip].inCart -= 1;
-    if(this.tripList[bookedTrip].inCart == 0){
-      this.tripsInCart.splice(this.tripsInCart.findIndex(x => x.id === ID), 1);
-    }
-  
-
-  }
-  
 }
