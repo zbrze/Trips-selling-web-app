ef8370a14a866f1e762ce90b74c1546123b8c401
zbrze <58784685+zbrze@users.noreply.github.com>
2020-12-26 00:49:43 +0100

    Add files via upload

--- src/app/one-trip/one-trip.component.ts
+++ src/app/one-trip/one-trip.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormGroup, FormBuilder}  from '@angular/forms';
 import { Validators } from '@angular/forms';
+import { AddTripService } from '../services/trip.service';
 import {Trip} from '../trip'
 
 @Component({
@@ -10,15 +11,13 @@ import {Trip} from '../trip'
 })
 export class OneTripComponent {
   tripForm;
-  @Output() newTrip = new EventEmitter<Trip>();
-  @Output() toggleMe = new EventEmitter<boolean>();
-  @Input() maxID: number;
-  @Input() showMePartially: boolean;
+  fromDate;
+  toDate
   isDate = '^(0?[1-9]|[12][0-9]|3[01])[/-](0?[1-9]|1[012])[/-]\\d{4}$';
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder, 
+    private service:AddTripService) {
     this.tripForm = this.formBuilder.group({
-      id: 0,
       name: ["", Validators.required],
       country: ["", Validators.required],
       from: ["", Validators.pattern(this.isDate)],
@@ -26,37 +25,48 @@ export class OneTripComponent {
       price : ["",  Validators.pattern("^\\d+\\.\\d{0,2}$")],
       places:["", Validators.required],
       description: [""],
-      URL: [""],
+      photoURL: [""],
       rating: 0.0,
       votes: 0,
       inCart: 0
     });
   } 
-  
+  getDateFrom(event){
+    this.fromDate = new Date(event.value);
+  }
+  getDateTo(event){
+    this.toDate = new Date(event.value);
+  }
   onSubmit(data){
+
     if(this.tripForm.valid){
-      this.showMePartially = ! this.showMePartially
-      data.id = this.maxID + 1;
-      data.from = new Date(data.from);
-      data.to = new Date(data.to);
-      data.price = parseFloat(data.price);
-      data.places = parseInt(data.places);
-      data.booked = 0;
       var today = new Date();
-      if(data.from < today){
+      if(this.fromDate< today){
         alert("You cant add a past trip");
         return;
       }
-      if(data.from > data.to){
+      if(this.fromDate >this.toDate){
         alert("Trip cant end before it starts");
         return;
       }
-      console.log(data);
       alert("trip added");
-      this.newTrip.emit(data);
+      data.booked = 0;
+      data.inCart = 0;
+      data.votes = 0;
+      data.rating = 3.0
+      data.price = parseFloat(data.price);
+      data.photoURL = data.photoURL;
+      data.description = data.description;
+      data.dateTo = this.toDate;
+      data.dateFrom = this.fromDate;
+      data.country = data.country;
+      data.name = data.name;
+      data.places = parseInt(data.places)
+
+      console.log(data)
+      this.service.addTrip(data);
 
       this.tripForm.reset();
-      data.appe
     }else{
       alert("Cant submit, invalid data");
       console.log(data);
