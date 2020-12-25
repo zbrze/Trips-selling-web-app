3bf31be317ee71bd0ed67ea0b6f34dace0881b01
zbrze <58784685+zbrze@users.noreply.github.com>
2020-12-26 00:52:40 +0100

    Add files via upload

--- src/app/trip-in-cart/trip-in-cart.component.ts
+++ src/app/trip-in-cart/trip-in-cart.component.ts
@@ -1,26 +1,26 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AddTripService } from '../services/trip.service';
 import {Trip} from '../trip'
 @Component({
   selector: 'app-trip-in-cart',
   templateUrl: './trip-in-cart.component.html',
   styleUrls: ['./trip-in-cart.component.css']
 })
-export class TripInCartComponent implements OnInit {
-  @Output() RemoveTrip = new EventEmitter<number>();
-  @Output() AddTrip = new EventEmitter<number>();
-  @Input() trip: Trip;
-  constructor() { }
+export class TripInCartComponent {
+  @Input() trip: any;
+
+  constructor(private service: AddTripService) { }
 
   ngOnInit(): void {
   }
 
   onRemove(){
     console.log("id wycieczki:" + this.trip.id);
-    this.RemoveTrip.emit(this.trip.id);
+    this.service.resign(this.trip.id);
   }
   onAdd(){
     console.log("id wycieczki:" + this.trip.id);
-    this.AddTrip.emit(this.trip.id);
+    this.service.book(this.trip.id)
   }
 
 }
