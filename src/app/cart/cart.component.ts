6442de87ccdbf1c7cb5d7497979c643de654e01b
zbrze <58784685+zbrze@users.noreply.github.com>
2020-12-26 00:47:40 +0100

    Add files via upload

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -2,29 +2,47 @@ import { NgStyle } from '@angular/common';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import {Trip} from '../trip';
 import { CommonModule } from '@angular/common';
+import { AddTripService } from '../services/trip.service';
+import { map } from 'rxjs/operators';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent{
-  @Input() trips : Trip[];
-  @Output() tripRemove = new EventEmitter<number>();
-  @Output() tripAdd = new EventEmitter<number>();
-  showMePartially = false;
-  constructor() { }
-  showCart(){
-    this.showMePartially = ! this.showMePartially
+  trips;
+  count
+  constructor(private service :AddTripService ) {
+    this.trips = [];
+  }
+
+  ngOnInit(){
+    this.getCart();
+    this.service.getCartSize().then(x => this.count = x.toString());
+    console.log()
+  }
+
+  getCart(){
+    this.service.
+    getCart().pipe(
+      map(c => c.map( val => 
+        ({id: val.payload.doc.id,...val.payload.doc.data()}))
+     )).
+    subscribe(x => this.trips = x,err=>console.log(err));
   }
   sumNum(){
-    return  this.trips.reduce((sum, trip) => sum + trip.inCart ,0)
+    console.log(this.trips);
+    let x = this.trips.reduce((sum, trip) => trip.inCart.valueOf() ,0)
+    if(x>0) return x;
+    else{
+      return 0;
+    }
+    console.log(this.trips);
   }
   removeFromCart(ID:number){
-    this.tripRemove.emit(ID);
     console.log("usuwam wycieczkę z koszyka" + ID);
   }
   bookPlace(ID:number){
-    this.tripAdd.emit(ID);
     console.log("dodaję wycieczkę do koszyka" + ID);
   }
 
