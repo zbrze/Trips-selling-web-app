30b31621b2ca8ca082896ab4577408f1bf5352df
zbrze <58784685+zbrze@users.noreply.github.com>
2021-01-15 00:03:04 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,15 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyBojhvot4_HB2uAT80lOY1lKXnjvBcd_tg",
-    authDomain: "trips-a5034.firebaseapp.com",
-    projectId: "trips-a5034",
-    storageBucket: "trips-a5034.appspot.com",
-    messagingSenderId: "554888039793",
-    appId: "1:554888039793:web:74daf710a7e475c66ffad0",
-    measurementId: "G-KHGQMPFF0H"
+    apiKey: ******,
+    authDomain: ******,
+    projectId: ******,
+    storageBucket: ******,
+    messagingSenderId: ******,
+    appId: ******,
+    measurementId: ******,
   }
 };
 
