072f0d55615b1238bf5f50f7734df266a12cf75d
zbrze <58784685+zbrze@users.noreply.github.com>
2021-01-15 00:02:48 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyBojhvot4_HB2uAT80lOY1lKXnjvBcd_tg",
-    authDomain: "trips-a5034.firebaseapp.com",
-    projectId: "trips-a5034",
-    storageBucket: "trips-a5034.appspot.com",
-    messagingSenderId: "554888039793",
-    appId: "1:554888039793:web:74daf710a7e475c66ffad0",
-    measurementId: "G-KHGQMPFF0H"
+    apiKey: ******,
+    authDomain: ******,
+    projectId: ******,
+    storageBucket: ******,
+    messagingSenderId: ******,
+    appId: ******,
+    measurementId: ******,
   }
 };
